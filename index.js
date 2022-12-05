6678c8c86d24d07f60e742d589f9eb5c7b03d073
Chris Webster <chriswebster1985@googlemail.com>
2022-12-05 18:35:42 +0000

    commit initial files

---
+++ index.js
@@ -0,0 +1,70 @@
+const homeScore = document.getElementById("homeScore");
+let scoreHome = 0
+
+function plus1home() {
+    console.log("button clicked");
+    scoreHome = scoreHome+1;
+    homeScore.textContent = scoreHome;
+}
+
+function plus2home() {
+    console.log("button clicked");
+    scoreHome = scoreHome+2;
+    homeScore.textContent = scoreHome;
+}
+
+function plus3home() {
+    console.log("button clicked");
+    scoreHome = scoreHome+3;
+    homeScore.textContent = scoreHome;
+}
+
+const guestScore = document.getElementById("guestScore");
+let scoreGuest = 0
+
+function plus1guest() {
+    console.log("button clicked");
+    scoreGuest = scoreGuest+1;
+    guestScore.textContent = scoreGuest;
+}
+
+function plus2guest() {
+    console.log("button clicked");
+    scoreGuest = scoreGuest+2;
+    guestScore.textContent = scoreGuest;
+}
+
+function plus3guest() {
+    console.log("button clicked");
+    scoreGuest = scoreGuest+3;
+    guestScore.textContent = scoreGuest;
+} 
+
+function newgame() {
+    scoreHome = 0;
+    scoreGuest = 0;
+    homeScore.textContent = scoreHome;
+    guestScore.textContent = scoreGuest;
+}
+
+let hlHome = document.getElementById("homeScore")
+
+function highlightHome() {
+    if(scoreHome > scoreGuest) {
+        hlHome.style.color = "white";
+        hlGuest.style.color = "#F94F6D";
+    } else {
+        hlHome.style.color = "#F94F6D";  
+    }
+}
+
+let hlGuest = document.getElementById("guestScore")
+
+function highlightGuest() {
+    if(scoreGuest > scoreHome) {
+        hlGuest.style.color = "white";
+        hlHome.style.color = "#F94F6D";
+    } else {
+        hlGuest.style.color = "#F94F6D";  
+    }
+}
